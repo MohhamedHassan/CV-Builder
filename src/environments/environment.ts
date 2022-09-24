4d0892a743849fd4de6419ee22e2ca53eb4e15ea
Mohamed Hassan <mohamed>
2022-09-24 13:02:39 +0200

    change api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  apiUrl:'https://simple-api11.herokuapp.com/'
+  apiUrl: 'https://cv-builder-api.onrender.com/'
 };
-
+ // old api url /// apiUrl:'https://simple-api11.herokuapp.com/'
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
